3602e0d3acafb12362507b8ffb4a12aabb9b71fd
Farhad Agzamov <farhad@thisistommy.com>
2018-03-12 23:40:33 +0000

    compoenents

--- src/Player.js
+++ src/Player.js
@@ -1,17 +1,47 @@
 import React, { Component } from 'react';
-import data from './data/data.js'
+import data from './data/data.js';
 
 class Player extends Component {
 	render() {
 		return (
 			<ul>
-				{data.map(function(name, index){
-					return <div className="Player">
-						<iframe title="video" width="560" height="315" src={"https://www.youtube.com/embed/" + name.link} frameBorder="0" allow="encrypted-media" allowFullScreen></iframe>
-					</div>
+				{data.map( (video) => {
+					return (
+						<div>
+							<Name
+								name={video.name}
+							/>
+							<Date
+								date={video.date}
+							/>
+							<div className="Player">
+								<iframe title="video" width="560" height="315" src={"https://www.youtube.com/embed/" + video.link} frameBorder="0" allow="encrypted-media" allowFullScreen></iframe>
+							</div>
+						</div>
+						)
 				})}
 			</ul>
 		);
+	}
+}
+
+class Name extends Component {
+	render() {
+		return (
+			<div>
+				<h3> {this.props.name} </h3>
+			</div>
+		)
+	}
+}
+
+class Date extends Component {
+	render() {
+		return (
+			<div>
+				<p>{this.props.date} </p>
+			</div>
+		)
 	}
 }
 
