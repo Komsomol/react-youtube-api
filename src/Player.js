4dd80a26652081ffaba40bef063eebfd28ff3823
Farhad Agzamov <farhad@thisistommy.com>
2018-03-14 18:23:14 +0000

    early work on fetching data

--- src/Player.js
+++ src/Player.js
@@ -3,25 +3,52 @@ import './Player.css';
 import data from './data/data.js';
 import ResponsiveEmbed from 'react-responsive-embed';
 
+
+
+
 class Player extends Component {
+	
+	constructor(props) {
+		super(props);
+		this.state = {
+			error:null,
+			isLoaded:false,
+			items:[]
+		};
+	}
+
+	componentDidMount(){
+		fetch('http://top-100-billboard.herokuapp.com/data/')
+		.then(response => response.json())
+		.then(result => {
+			console.log(typeof result)
+			this.setState({
+				isLoaded: true,
+				items: result
+			},
+			(error) => {
+				this.setState({
+					isLoaded :true,
+					error:error
+				})
+			})
+		})
+	}
+
 	render() {
-		return (
-			<ul>
-				{data.map( (video) => {
-					return (
-						<div className="flex-container">
-							<ResponsiveEmbed src={"https://www.youtube.com/embed/" + video.link} ratio='16:9'/>
-							<Name
-								name={video.name}
-							/>
-							<Date
-								date={video.date}
-							/>
-						</div>
-						)
-				})}
-			</ul>
-		);
+		const {error, isLoaded, items} = this.state;
+		console.log(items);
+		if(error){
+			return  (<div> Erorr : {error} </div>);
+		} else if (!isLoaded){
+			return  (<div>Loading...</div>)
+		} else {
+			return (
+				<ul>
+					{items[0]}
+				</ul>
+			)
+		}
 	}
 }
 
@@ -44,5 +71,20 @@ class Date extends Component {
 		)
 	}
 }
+
+
+					// {items.map( (song) => {
+					// 	return (
+					// 		<div className="flex-container">
+					// 			<ResponsiveEmbed src={"https://www.youtube.com/embed/" + song.id} ratio='16:9'/>
+					// 			<Name
+					// 				name={song.title}
+					// 			/>
+					// 			<Date
+					// 				date={song.rank}
+					// 			/>
+					// 		</div>
+					// 		)
+					// })}
 
 export default Player;
